95e5ff6546cc2a950c7d0dcab6a0501ceccc481d
MishaNzn <nie.misha.ua@gmail.com>
2021-09-18 21:30:45 +0300

    Animation

--- Happy Chicken.py
+++ Happy Chicken.py
@@ -14,12 +14,15 @@ sprites = []
 STAND = 0
 GO = 1
 action = STAND
-images_left = [PhotoImage(file='ChickenLeft1.png'), PhotoImage(file='chickenLeft2.png')]
-images_right = [PhotoImage(file='Chicken1.png'), PhotoImage(file='chicken2.png')]
+images_left = [PhotoImage(file='chickenLeft1.png'), PhotoImage(file='chickenLeft2.png')]
+images_right = [PhotoImage(file='chicken1.png'), PhotoImage(file='chicken2.png')]
 chicken = c.create_image(WIDTH / 2, HEIGHT / 2, image=images_left[0], anchor='nw')
-egg_file = PhotoImage(file='egg1.png')
+egg_file = [PhotoImage(file='egg1.png'), PhotoImage(file='egg2.png')]
 ch_x = WIDTH / 2
 ch_y = HEIGHT / 2
+score = 0
+class GameElement:
+    pass
 
 
 def change(image):
@@ -33,7 +36,11 @@ def change(image):
 
 
 def create_egg():
-    egg = c.create_image(ch_x + 10, ch_y + 40, image=egg_file)
+    egg = c.create_image(ch_x + 10, ch_y + 40, image=egg_file[0])
+    global score
+    score += 1
+    c.update_idletasks()    
+    c.itemconfig(egg, image=egg_file[1])
     c.update_idletasks()
 
 
@@ -67,7 +74,9 @@ def move_chicken(event):
 
 
 c.bind_all('<Key>', move_chicken)
+score_text = c.create_text(250, 30, fill='yellow', font=('Comic Sans MS', 12))
 while 1:
     c.update_idletasks()
     c.update()
     time.sleep(0.01)
+    c.itemconfig(score_text, text='Score: %s' % score)
